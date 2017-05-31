436a4f66c9d60621f08f4401640f8938bff040dc
Martins Talbergs <animaacija@gmail.com>
2017-05-31 22:13:28 +0300

    cmd + c fix
    Fixes OSX issue when not able to copy text from debugger UI

--- electron/main.js
+++ electron/main.js
@@ -26,6 +26,23 @@ function createWindow () {
     mainWindow.webContents.openDevTools()
   }
 
+  electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate([{
+    label: "Application",
+    submenu: [
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+  ]))
+
   electron.ipcMain.on('message', function (event, payload) {
     if (!clients[payload.port] || !clients[payload.port].ws) {
       return
