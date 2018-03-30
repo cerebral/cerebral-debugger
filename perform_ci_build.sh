e8fd05032521775fe4974179a2e6a283e4994510
Henri Hulski <henri.hulski@gazeta.pl>
2018-03-30 16:40:58 +0200

    Launch snapd directly

--- perform_ci_build.sh
+++ perform_ci_build.sh
@@ -3,7 +3,7 @@ git config --global user.email "christianalfoni@gmail.com"
 git config --global user.name "Christian Alfoni"
 if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then npm run build;
   if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    systemctl restart snapd.service
+    /usr/lib/snapd/snapd
     snap install snapcraft --classic
     npm run package:linux -- --publish=always;
   fi
