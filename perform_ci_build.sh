ddc9f380611138051273d5df355dfc7b6534e480
Henri Hulski <henri.hulski@gazeta.pl>
2018-03-30 16:50:31 +0200

    Use node 8 and remove snapd in Travis build

--- perform_ci_build.sh
+++ perform_ci_build.sh
@@ -3,8 +3,6 @@ git config --global user.email "christianalfoni@gmail.com"
 git config --global user.name "Christian Alfoni"
 if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then npm run build;
   if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    /usr/lib/snapd/snapd
-    snap install snapcraft --classic
     npm run package:linux -- --publish=always;
   fi
   if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
